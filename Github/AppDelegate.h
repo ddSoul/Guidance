5be7dd421494804df434a2042b9682896f6b7b0d
ddSoul <972424506@qq.com>
2017-09-10 10:18:58 +0800

    first commit

---
+++ Github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github
+//
+//  Created by ddSoul on 2017/9/10.
+//  Copyright © 2017年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
